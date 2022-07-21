adcaa8bcacf20460a79b2ce4739b34f7653dbb40
STepsic99 <tepsic.ra68.2018@uns.ac.rs>
2022-07-21 22:49:26 +0200

    feat: set up project

---
+++ DoctorsOffice/src/main/java/com/doctorsoffice/DoctorsOfficeApplication.java
@@ -0,0 +1,13 @@
+package com.doctorsoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctorsOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctorsOfficeApplication.class, args);
+	}
+
+}
